812d7ff821c03d035d79a4692b303f760a29e924
Johan Vandegriff <johan@vandymail.com>
2022-08-20 02:09:15 -0400

    also remove old files when building gallery

--- build-gallery.sh
+++ build-gallery.sh
@@ -2,7 +2,11 @@
 cd `dirname $0`
 
 sed -i 's,ignoreFiles = \["gallery/.*"\],#ignoreFiles = ["gallery/.*"],g' config.toml
+sed -i 's,cleanDestinationDir = false,cleanDestinationDir = true,g' config.toml
 scp config.toml johanv.net:hugo/config.toml
+
 sleep 2
+
 sed -i 's,#ignoreFiles = \["gallery/.*"\],ignoreFiles = ["gallery/.*"],g' config.toml
+sed -i 's,cleanDestinationDir = true,cleanDestinationDir = false,g' config.toml
 scp config.toml johanv.net:hugo/config.toml
