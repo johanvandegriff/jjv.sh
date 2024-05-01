14286a0d0832453bb14c255e2a345288e9b4ea49
Johan Vandegriff <johan@vandymail.com>
2024-05-01 01:22:23 -0400

    fix path for new deployment method

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -aP --itemize-changes --delete ~/git/johanvandegriff/johanv.net/ johanv.net:hugo/ --exclude public --exclude resources/_gen $@
+rsync -aP --itemize-changes --delete ~/git/johanvandegriff/johanv.net/ johanv.net:johanv.net/hugo/ --exclude public --exclude resources/_gen $@
